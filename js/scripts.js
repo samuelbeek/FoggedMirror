136fc5281d57d2f1ee09d09f20daf002817f7d72
Samuel Beek <ik@samuelbeek.com>
2014-05-19 16:41:15 +0200

    FoggedMirror
    Web application (now only supports firefox and chrome) that enables you
    to write on a fogged mirror, displaying you (off course)

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Script.js - Made by Samuel Beek
+var video = document.querySelector("#videoElement");
+ 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+if (navigator.getUserMedia) {      
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+ 
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+ 
+function videoError(e) {
+    // do something
+}
